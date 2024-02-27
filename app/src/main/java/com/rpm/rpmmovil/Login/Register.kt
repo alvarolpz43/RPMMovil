ca4efad81410f1a4bdf074e089da71211f3fdccc
Camilo Tacue Salazar <cetacue@misena.edu.co>
2024-02-27 00:33:32 -0500

    register 90%

--- app/src/main/java/com/rpm/rpmmovil/Login/Register.kt
+++ app/src/main/java/com/rpm/rpmmovil/Login/Register.kt
@@ -19,7 +19,7 @@ class Register : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
 
  val retrofit = Retrofit.Builder()
-        .baseUrl("https://rpm-back-end.vercel.app/api/")  // Replace with your actual base URL
+        .baseUrl("https://rpm-back-end.vercel.app/api/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
@@ -38,47 +38,46 @@ class Register : AppCompatActivity() {
     }
 
     private fun listenerevents() {
-        binding.BtnBack.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        }
+
 
         binding.BtnRegister.setOnClickListener {
             val nombre = binding.ETName.text.toString()
             val email = binding.ETEmail.text.toString()
             val password = binding.ETPassword.text.toString()
+            val identificationNumber = binding.ETIdentificacion.text.toString().toInt()
+            val phoneNumber = binding.ETTelefono.text.toString().toLong()
 
             val userRegistrationData = DtaRegister(
                 Nombres_Mv = nombre,
                 Email_Mv = email,
-                NumeroIdent_Mv = 123456789,  // Replace with the actual identification number
-                FechaNac_Mv = "1990-01-01",  // Replace with the actual date of birth
+                NumeroIdent_Mv = identificationNumber,
+                FechaNac_Mv = "1990-01-01",
                 Contraseña_Mv = password,
-                NumeroTel_Mv = 1234567890  // Replace with the actual telephone number
+                NumeroTel_Mv = phoneNumber
             )
 
-            // Make the registration request
+
             val call: Call<DtaRegister> = apiService.getRegister(userRegistrationData)
 
             call.enqueue(object : Callback<DtaRegister> {
                 override fun onResponse(call: Call<DtaRegister>, response: Response<DtaRegister>) {
                     if (response.isSuccessful) {
-                        val intent = Intent(this@Register,MainActivity::class.java)
-                        // Process the response body if needed
+
+                        val intent = Intent(this@Register, MainActivity::class.java)
                         Toast.makeText(applicationContext, "Registro exitoso", Toast.LENGTH_SHORT).show()
                         startActivity(intent)
                     } else {
-                        // Handle unsuccessful response
+
                         Toast.makeText(applicationContext, "Error en el registro", Toast.LENGTH_SHORT).show()
                     }
                 }
 
                 override fun onFailure(call: Call<DtaRegister>, t: Throwable) {
-                    // Handle failure
+
                     Toast.makeText(applicationContext, "Error en el registro", Toast.LENGTH_SHORT).show()
                 }
             })
         }
+
     }
 }
