371f421afa8e4ce73e831a6feef6d43d871fc94a
unknown <martinesj405@gmail.com>
2024-03-12 14:32:18 -0500

    update cmpos

--- app/src/main/java/com/rpm/rpmmovil/Routes/ListarRutasActivity.kt
+++ app/src/main/java/com/rpm/rpmmovil/Routes/ListarRutasActivity.kt
@@ -13,21 +13,10 @@ class ListarRutasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityListarRutasBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val manager = ManagerDb(this)
-        val arrayRoute = manager.getData()
 
-        // Crear una lista de nombres de rutas
-        val nombreDeRutas = arrayRoute.map { it.rutaN }
-
-        val listRoute = binding.listaRutas
-        val arrayAdapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, nombreDeRutas)
-
-        listRoute.adapter = arrayAdapter
-        Toast.makeText(this, "Rutas listadas", Toast.LENGTH_SHORT).show()
     }
 
 
